46d44fcb75c8999aedfc97868f27a3383abaaf24
nekomikandayo <mrmikan.1417@gmail.com>
2026-04-04 01:37:50 +0900

    chore: add yudem code

--- WebDevelopmentBootcamp/AJAX_CODE/Axios/app.js
+++ WebDevelopmentBootcamp/AJAX_CODE/Axios/app.js
@@ -15,5 +15,50 @@ const getStarWarsPerson = async (id) => {
     }
 }
 
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+// axios.get('https://swapi.dev/api/people/1/')
+//      .then((res) => {
+//         console.log('RESPONSE!', res);
+//      })
+//      .catch((e) => {
+//         console.log('ERROR!!!', e);
+//      });
+
+// const getStarWarsPerson = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log('ERROR', e)
+//     }
+// }
+
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+const jokes = document.querySelector('#jokes');
+
+const getDadJoke = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        console.log(res.data);
+    } catch (e) {
+        console.log('ERROR', e);
+    }
+    
+    return res.data.joke;
+}
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.innerText = jokeText;
+    jokes.append(newLI);
+}
