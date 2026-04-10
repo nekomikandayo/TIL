90ce64e502f4ae11c88acdaf15cd83a2160e16de
nekomikandayo <mrmikan.1417@gmail.com>
2026-04-10 22:39:33 +0900

    モジュールの練習

---
+++ WebDevelopmentBootcamp/practice/NODE_JS2/app.js
@@ -0,0 +1,6 @@
+const {PI, square, add} = require('./math');
+
+console.log(add(PI, 10));
+console.log(square(9));
+
+
---
+++ WebDevelopmentBootcamp/practice/NODE_JS2/math.js
@@ -0,0 +1,19 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+export {add, PI, square};
+
+
+// const math = module.exports = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+// module.exports = math;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
