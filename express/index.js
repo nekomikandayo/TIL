38ac172e463c7cc360d7359c742f84523a4b8b0b
nekomikandayo <mrmikan.1417@gmail.com>
2026-04-14 22:35:27 +0900

    new express

---
+++ express/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+// app.use((req, res) =>{
+//     console.log('We got a new request!');
+//     res.send({color: 'red', food: 'banana'});
+// });
+
+app.get('/cats', (req, res) =>{
+    res.send('MEOW');
+});
+
+app.post('/cats', (req, res) =>{
+    res.send('POST REQUEST TO /cats!! THIS IS DIFFERENT THAN GET');
+});
+
+
+
+app.get('/dogs', (req, res) =>{
+    res.send('WOOOF');
+});
+
+app.get('/', (req, res) =>{
+    res.send('This is the home page');
+});
+
+app.get('/r/:subreddit/:postId', (req, res) =>{
+    const {subreddit, postId} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1><h2>Viewing post ID: ${postId}</h2>`);
+});
+
+app.use((req, res) =>{
+    res.status(404).send('Not Found');
+});
+
+
+
+app.listen(3000, () =>{
+    console.log('Listening on port 3000');
+});
